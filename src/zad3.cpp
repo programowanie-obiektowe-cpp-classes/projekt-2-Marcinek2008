3d45dd43f13b921a89ead4bd74ec261e79b783e4
Marcinek2008 <ms2408@op.pl>
2025-12-18 19:32:40 +0100

    Add files via upload

--- src/zad3.cpp
+++ src/zad3.cpp
@@ -3,96 +3,62 @@
 #include <vector>
 #include <string>
 #include <sstream>
-#include <algorithm>
 #include <print>
 
-struct Point {
-	double x, y;
-	double vx, vy;
+struct Claim {
+    int id;
+    int x, y;
+    int w, h;
 };
 
+
+
 int main() {
-	std::ifstream file("input3.txt");
-	if (!file) {
-		throw std::runtime_error("Nie mozna otworzyc pliku");
-	}
-	std::vector<Point> punkty;
-	std::string line;
+    std::ifstream file("input2.txt");
+    if (!file) {
+        throw std::runtime_error("Nie mozna otworzyc pliku");
+    }
+    std::vector<Claim> claims;
+    std::string line;
+    while (std::getline(file, line)) {
+        int id, x, y, w, h;
+        char ch1, ch2, ch3, ch4;
+        std::stringstream ss(line);
+        ss >> ch1 >> id >> ch2 >> x >> ch3 >> y >> ch4 >> w >> ch4 >> h;
+        claims.push_back({ id, x, y, w, h });
+    }
 
-	while (std::getline(file, line)) {
-		for (char& c : line) {
-			if (!(isdigit(c) || c == '-'))
-				c = ' ';
-			
-		}
+    int rozmiar = 1000;
+    std::vector<std::vector<int>> plotno(rozmiar, std::vector<int>(rozmiar, 0));
 
-		std::stringstream ss(line);
-		Point p;
-		ss >> p.x >> p.y >> p.vx >> p.vy;
-		punkty.push_back(p);
-	}
+    for (const auto& c : claims) {
+        for (int i = 0; i < c.w; ++i) {
+            for (int j = 0; j < c.h; ++j) {
+                plotno[c.x + i][c.y + j] +=1;
+            }
+        }
+    }
+    int nakladanie = 0;
+    for (int i = 0; i < rozmiar; ++i)
+        for (int j = 0; j < rozmiar; ++j)
+            if (plotno[i][j] > 1)
+                nakladanie++;
 
-	double popPole = 100000000000000000;
-	int t = 0;
+    std::println("{}", nakladanie);
+    for (const auto& c : claims) {
+        bool ok = true;
+        for (int i = 0; i < c.w && ok; ++i) {
+            for (int j = 0; j < c.h && ok; ++j) {
+                if (plotno[c.x + i][c.y + j] > 1) {
+                    ok = false;
+                }
+            }
+        }
+        if (ok) {
+            std::println("{}",c.id);
+            break;
+        }
+    }
 
-	while (true) {
-		for (auto& p : punkty) {
-			p.x += p.vx;
-			p.y += p.vy;
-		}
-		t += 1;
-
-		double minX = punkty[0].x;
-		double minY = punkty[0].y;
-		double maxX = punkty[0].x;
-		double maxY = punkty[0].y;
-		for (auto& p : punkty) {
-			minX = std::min(minX, p.x);
-			minY = std::min(minY, p.y);
-			maxX = std::max(maxX, p.x);
-			maxY = std::max(maxY, p.y);
-		}
-
-		double pole = (maxX - minX) * (maxY - minY);
-
-		if (pole > popPole) {
-			for (auto& p : punkty) {
-				p.x -= p.vx;
-				p.y -= p.vy;
-			}
-			t -= 1;
-			break;
-		}
-		popPole = pole;
-	}
-		double minX = punkty[0].x;
-		double minY = punkty[0].y;
-		double maxX = punkty[0].x;
-		double maxY = punkty[0].y;
-	for (const auto& p : punkty) {
-		minX = std::min(minX, p.x);
-		maxX = std::max(maxX, p.x);
-		minY = std::min(minY, p.y);
-		maxY = std::max(maxY, p.y);
-	}
-
-	int szerokosc = int(maxX - minX + 1);
-	int wysokosc = int(maxY - minY + 1);
-
-	std::vector<std::string> siatka(wysokosc, std::string(szerokosc, '.'));
-
-	for (const auto& p : punkty) {
-		int x = int(p.x - minX);
-		int y = int(p.y - minY);
-		if (x >= 0 && x < szerokosc && y >= 0 && y < wysokosc)
-			siatka[y][x] = '#';
-	}
-
-
-	for (const auto& wiersz : siatka)
-	{
-		std::println("{}", wiersz);
-	}
-	std::println("Czas: {}", t);
-	return 0;
+    return 0;
 }
